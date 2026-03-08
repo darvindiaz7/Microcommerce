00f99a11a95771e0c2e346982242a40f299dd4d6
Darvin Diaz <darwindiazpaima@gmail.com>
2026-03-08 14:55:49 -0500

    delete prueba

--- Usuarios/src/main/java/Usuarios/UsuariosApplication.java
+++ Usuarios/src/main/java/Usuarios/UsuariosApplication.java
@@ -8,7 +8,8 @@ public class UsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsuariosApplication.class, args);
-		System.out.println("prueba");
+
+		System.out.println("elpepe");
 	}
 
 }
